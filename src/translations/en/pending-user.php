b3e126e82bc978ae7abcac44e0305d9d148aff90
Andrea DeMers <andrea@andreademers.com>
2020-10-23 19:17:51 -0400

    Change order of system messages to match those in Craft CP

--- src/translations/en/pending-user.php
+++ src/translations/en/pending-user.php
@@ -26,20 +26,19 @@ return [
 
     // Messages
     // User registers and thereby creates a new user account
-    'user_registered_heading' => 'Pending User plugin: when a new user registers',
-    'user_registered_subject' => '{{siteName}}: User account created',
+    'user_registered_heading' => 'Pending User: when a new user registers',
+    'user_registered_subject' => 'User account created',
     'user_registered_body' => "Hi {{user.friendlyName}},\n\n" .
-        "Your {{siteName}} user account {{user.username}} has been created and is pending activation.\n\n" .
+        "Your {{systemName}} user account {{user.username}} has been created and is pending activation.\n\n" .
         "You'll receive another email once we've reviewed and activated your account.\n\n",
-    // User account activated by moderator
-    'user_activated_heading' => 'Pending User plugin: when a user account is activated',
-    'user_activated_subject' => '{{siteName}}: User account activated',
-    'user_activated_body' => "Hi {{user.friendlyName}},\n\n" .
-        "Your {{siteName}} user account {{user.username}} has been activated.\n\n",
     // Moderator email notification
-    'moderator_notification_heading' => 'Pending User plugin: when the Moderator Email Notification setting is enabled',
-    'moderator_notification_subject' => '{{siteName}}: User account activation request',
+    'moderator_notification_heading' => 'Pending User: when the Moderator Email Notification setting is enabled',
+    'moderator_notification_subject' => 'User account activation request',
     'moderator_notification_body' => "Hi Moderator,\n\n" .
-        "Please visit {{user.cpEditUrl}} to review and activate the {{user.username}} user account.\n\n"
-
+        "Please visit {{user.cpEditUrl}} to review and activate the {{user.username}} user account.\n\n",
+    // User account activated by moderator
+    'user_activated_heading' => 'Pending User: when a user account is activated',
+    'user_activated_subject' => 'User account activated',
+    'user_activated_body' => "Hi {{user.friendlyName}},\n\n" .
+        "Your {{systemName}} user account {{user.username}} has been activated.\n\n"
 ];
